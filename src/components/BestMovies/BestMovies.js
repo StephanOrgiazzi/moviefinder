3e022f48d7cf57d16d5dee24461f5961b6432e9f
stephan <stephan.orgiazzi@gmail.com>
2018-05-01 13:20:50 +0200

    added responsive slider

--- src/components/BestMovies/BestMovies.js
+++ src/components/BestMovies/BestMovies.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styles from './BestMovies.module.css';
 import axios from 'axios';
+import Slider from "react-slick";
 
 class BestMovies extends Component {
 
@@ -23,18 +24,74 @@ class BestMovies extends Component {
     }
 
     render() {
+
+        const settings = {
+            dots: false,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            touchThreshold: 10,
+            responsive: [
+                {
+                    breakpoint: 2024,
+                    settings: {
+                        dots: false,
+                        slidesToShow: 4,
+                        slidesToScroll: 4,
+                        infinite: true,
+                        centerPadding: '100px',
+                    }
+                },
+                {
+                    breakpoint: 900,
+                    settings: {
+                        dots: false,
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                    }
+                },
+                {
+                    breakpoint: 700,
+                    settings: {
+                        dots: false,
+                        arrows: true,
+                        slidesToShow: 3,
+                        centerMode: false,
+                        slidesToScroll: 3,
+                    }
+                },
+                {
+                    breakpoint: 580,
+                    settings: {
+                        dots: false,
+                        arrows: false,
+                        slidesToShow: 3,
+                        centerMode: false,
+                        slidesToScroll: 3,
+                    }
+                }
+            ]
+        };
+
         return (
             <div className={styles.BestMovies}>
                 <h1>Les 10 meilleurs films</h1>
-                {this.state.bestMovieList.map(movie => {
-                    return (
-                        <div key={movie.id} className={styles.bestMovie}>
-                            <img src={`https://image.tmdb.org/t/p/w154/${movie.poster_path}`} alt={`${movie.title} poster`}/>
-                            <div className={styles.bestTitle}>{movie.title}</div>
-                            <div className={styles.bestYear}>{movie.release_date.slice(0,4)}</div>
-                        </div>
-                    )
-                })}
+                {this.state.bestMovieList.length > 1 &&
+                    <Slider {...settings}>
+                        {
+                            this.state.bestMovieList.map(movie => {
+                                return (
+                                    <div key={movie.id} className={styles.bestMovie}>
+                                        <img src={`https://image.tmdb.org/t/p/w154/${movie.poster_path}`} alt={`${movie.title} poster`} />
+                                        <div className={styles.bestTitle}>{movie.title}</div>
+                                        <div className={styles.bestYear}>{movie.release_date.slice(0, 4)}</div>
+                                    </div>
+                                )
+                            })
+                        }
+                    </Slider>
+                }
             </div>
         );
     }
